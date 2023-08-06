9a93b6a2481e774651ed8f105ea566d6441bddca
Rick Umali <rumali@veracode.com>
2023-08-06 14:52:12 -0400

    Changes App.js with initial tutorial code
    The output can be seen in the console.log (Dev Tools). This returns
    5 objects from the API.
    
    [
        {
            "name": "bulbasaur",
            "url": "https://pokeapi.co/api/v2/pokemon/1/"
        },
        {
            "name": "ivysaur",
            "url": "https://pokeapi.co/api/v2/pokemon/2/"
        },
        {
            "name": "venusaur",
            "url": "https://pokeapi.co/api/v2/pokemon/3/"
        },
        {
            "name": "charmander",
            "url": "https://pokeapi.co/api/v2/pokemon/4/"
        },
        {
            "name": "charmeleon",
            "url": "https://pokeapi.co/api/v2/pokemon/5/"
        }
    ]

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 import "./styles.css";
 
+import { useEffect, useState } from "react";
+import { Pagination } from "react-bootstrap";
+import axios from "axios";
+
 export default function App() {
+  const [limit, setLimit] = useState(5);
+  const [backData, setBackData] = useState([]);
+  const [data, setData] = useState([]);
+
+  const loadData = (numberOfRecords) => {
+    axios
+      .get(
+        `https://pokeapi.co/api/v2/pokemon?limit=${numberOfRecords}&offset=${backData.length}`
+      )
+      .then(({ data }) => console.log(data))
+      .catch((err) => console.log(err));
+  };
+
+  useEffect(() => {
+    loadData(5);
+  }, []);
+
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <div
+      style={{
+        padding: "30px",
+        backgroundColor: "#34495e",
+        color: "white",
+        marginBottom: "20px",
+      }}
+    ></div>
   );
 }
