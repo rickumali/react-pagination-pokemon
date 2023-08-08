7e8c4d9a75ae8302682f09376fbc7527c990524d
Rick Umali <rickumali@gmail.com>
2023-08-07 20:54:35 -0400

    Adds pageClick function
    This makes the links active, and the pagination somewhat works!

--- src/App.js
+++ src/App.js
@@ -12,6 +12,19 @@ export default function App() {
   const [active, setActive] = useState(1);
   const [next, setNext] = useState(2);
 
+  const pageClick = async (pageNumber) => {
+    pageNumber = pageNumber <= 0 ? 1 : pageNumber;
+
+    if (backData.length < pageNumber * limit) {
+      await loadData(pageNumber * limit, pageNumber);
+    } else {
+      setData(backData.slice((pageNumber - 1) * limit, pageNumber * limit));
+    }
+    setActive(pageNumber);
+    if (pageNumber >= next + 8) setNext(next + 1);
+    if (pageNumber !== 1 && pageNumber <= next) setNext(next - 1);
+  };
+
   const PaginationComponent = (next) => {
     let numbers = [];
     for (
